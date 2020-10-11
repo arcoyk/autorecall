69a87413adbaca233f7498f344e517a67fe784ba
yui <yuikita21@gmail.com>
2020-10-12 01:18:35 +0900

    basic scraping by headless chrome on macos

--- chrome/test.py
+++ chrome/test.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 options = Options()
 options.add_argument("--headless")
-driver = webdriver.Chrome("./chromedriver_mac_85", options=options)
+driver = webdriver.Chrome("./chromedriver", options=options)
 for animal in ["Dog", "Cat", "Bird", "Fish"]:
     driver.get("https://en.wikipedia.org/wiki/" + animal)
     print(driver.title)
