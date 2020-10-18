63710b480d3a24ba76d2b5e3b75f9e402cafa798
yui <yuikita21@gmail.com>
2020-10-18 21:55:59 +0900

    fix

--- chrome/app/test.py
+++ chrome/app/test.py
@@ -1,14 +1,18 @@
-from flask import Flask, request
-from image_search import ImageSearch
+from flask import Flask, request, make_response, jsonify
+# from image_search import ImageSearch
 import json
 
 app = Flask(__name__)
-driver = "/usr/bin/chromedriver"
+# driver = "/usr/bin/chromedriver"
 # driver = "../chromedriver"
-ims = ImageSearch(driver, headless=True)
-
-@app.route("/", methods=["POST"])
+# ims = ImageSearch(driver, headless=True)
+@app.route("/", methods=["GET"])
 def index():
-	q = request.json["query"]
-	d = json.dumps(ims.search(q))
-	return d
+	return "Access /data by POST to get data"
+
+@app.route("/data", methods=["POST"])
+def index():
+	r = make_response(jsonify({"hoge":43, "fuga": "ga"}), 200)
+	# q = request.json["query"]
+	# d = json.dumps(ims.search(q))
+	return r
